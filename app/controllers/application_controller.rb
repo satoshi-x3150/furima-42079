4d1396f379af147a639e94839ab0384ce3256f30
satoshi-x3150 <allex0toyota@gmail.com>
2025-03-04 17:59:21 +0900

    basic認証

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+  # アプリケーション全体に対して基本認証を適用するためのフィルタ
+  before_action :basic_auth
+
+
+  private
+
+  # 基本認証の設定
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == 'admin' && password == '2222'
+    end
+  end
 end
