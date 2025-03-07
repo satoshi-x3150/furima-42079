9a6ba6093310f10019f5ee0bf06dbb2d49ca64f4
satoshi-x3150 <allex0toyota@gmail.com>
2025-03-07 22:40:44 +0900

    アクティブハッシュ・アソシエーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,12 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+
+  # ActiveHash のアソシエーション設定
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :shipping_fee
+  belongs_to :prefecture
+  belongs_to :shipping_time
 end
