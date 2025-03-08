cb315d8aa941fd653e106cf473a75ede58856906
satoshi-x3150 <allex0toyota@gmail.com>
2025-03-08 11:46:08 +0900

    出品ページの編集・コントローラーの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,9 +1,31 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create]
   # before_action :authenticate_user!, except: [:index, :show]
 
   def index
   end
 
   def new
+    @item = Item.new
   end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path, notice: '商品を出品しました。'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(
+      :name, :description, :price, :image,
+      :category_id, :condition_id, :shipping_fee_id,
+      :prefecture_id, :shipping_time_id
+    ).merge(user_id: current_user.id)
+  end
+
 end
