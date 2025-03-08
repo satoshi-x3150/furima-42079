9d163a2388f6bab2f5e3c0310bc855a3b5c8302f
satoshi-x3150 <allex0toyota@gmail.com>
2025-03-08 17:23:35 +0900

    テストコード作成

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,13 +1,17 @@
 FactoryBot.define do
   factory :item do
-    name { "MyString" }
-    description { "MyText" }
-    price { 1 }
-    category_id { 1 }
-    condition_id { 1 }
-    shipping_fee_id { 1 }
-    prefecture_id { 1 }
-    shipping_time_id { 1 }
-    user { nil }
+    name { "テスト商品" }
+    description { "これはテスト用の商品です。" }
+    price { 1000 }
+    category_id { 2 } # 1以外（バリデーションで「---」は1となっているため）
+    condition_id { 2 }
+    shipping_fee_id { 2 }
+    prefecture_id { 2 }
+    shipping_time_id { 2 }
+    association :user
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
