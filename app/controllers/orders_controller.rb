eb1057b8abff457503f8cc2264a51c8558ac243f
satoshi-x3150 <allex0toyota@gmail.com>
2025-03-11 22:23:45 +0900

    エラー表示

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,17 +1,32 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_item  # `@item` をセット
+  before_action :set_item 
 
   def index
     @purchase_form = PurchaseForm.new
   end
 
   def create
+    @purchase_form = PurchaseForm.new(purchase_params)
+  
+    if @purchase_form.valid?
+      @purchase_form.save
+      redirect_to root_path
+    else
+      render :index, status: :unprocessable_entity
+    end
   end
 
   private
 
   def set_item
     @item = Item.find(params[:item_id])  # item_id を取得
+  end
+
+  def purchase_params
+    params.require(:purchase_form).permit(
+      :postal_code, :prefecture_id, :city, :address, 
+      :building_name, :phone_number
+    ).merge(user_id: current_user.id, item_id: params[:item_id])
   end
 end
