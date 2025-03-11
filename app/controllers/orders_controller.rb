184f1807bd3cf74dfc664f4eb28c1c49d6430b60
satoshi-x3150 <allex0toyota@gmail.com>
2025-03-11 21:38:05 +0900

    フォームの実装。

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,10 +1,17 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_item  # `@item` をセット
 
   def index
     @purchase_form = PurchaseForm.new
   end
 
   def create
+  end
+
+  private
+
+  def set_item
+    @item = Item.find(params[:item_id])  # item_id を取得
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "items#index"
   resources :items
-end
+
+  resources :items do
+    resources :orders, only: [:index, :create] # ← 購入関連のルーティングをネスト
+  end
+  end
